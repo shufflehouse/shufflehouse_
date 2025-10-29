04b3051215008d56db8602a234c16142964bfa45
shufflehouse <148178878+shufflehouse@users.noreply.github.com>
2025-10-29 13:18:36 -0400

    Update remove-renew.js

--- remove-renew.js
+++ remove-renew.js
@@ -1,9 +1,11 @@
-function removeRenewParagraphs() {
+function hideRenewParagraphs() {
   document.querySelectorAll('.form-group p').forEach(p => {
-    if (p.textContent.includes('Renews')) p.remove();
+    if (p.textContent.includes('Renews')) {
+      p.style.display = 'none';
+    }
   });
 }
 
-document.addEventListener('DOMContentLoaded', removeRenewParagraphs);
-document.addEventListener('click', removeRenewParagraphs);
-setInterval(removeRenewParagraphs, 1000);
+document.addEventListener('DOMContentLoaded', hideRenewParagraphs);
+document.addEventListener('click', hideRenewParagraphs);
+setInterval(hideRenewParagraphs, 1000);
