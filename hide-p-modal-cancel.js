2e4fea12620f19f61f56a09e0e3ba1d96d7fcc0d
shufflehouse <148178878+shufflehouse@users.noreply.github.com>
2026-06-15 08:21:51 -0400

    Update hide-p-modal-cancel.js

--- hide-p-modal-cancel.js
+++ hide-p-modal-cancel.js
@@ -8,7 +8,7 @@
           Before cancelling, we’d love to hear from you first.<br><br>
           If it’s about cost, timing, or anything we can do to improve your experience, please reach out —
           we’d genuinely love to help as humans, not bots.<br><br>
-          <a href="mailto:hello@shufflehouse.co?subject=🫶%20Question%20about%20cancellation"
+          <a href="mailto:hello@shufflehouse.co?subject=🫶%20Question%20about%20cost"
              style="color:#b3040f; text-decoration:underline;">
              Email hello@shufflehouse.co
           </a>
